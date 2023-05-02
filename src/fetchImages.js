0ec26ed5f252d8d2bc6baf3f1221ae4afe848792
valeri143 <valeria23200523251605@gmail.com>
2023-05-02 20:46:35 +0300

    changes

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -2,18 +2,7 @@ import axios from "axios";
 async function fetchImages (value, page=1){
     const BASE_URL = "https://pixabay.com/api/";
     const API_KEY = "35835023-fcd630b3e0ba098d72230f346";
-    // const param = new URLSearchParams({
-    //     key : API_KEY,
-    //     q : value,
-    //     image_type : "photo",
-    //     orientation : "horizontal",
-    //     safesearch : true,
-    //     per_page : 40,
-    //     page : page,
-    // })
-    
- return  axios.get(`${BASE_URL}`, {
-    params: {
+    const params = new URLSearchParams({
         key : API_KEY,
         q : value,
         image_type : "photo",
@@ -21,12 +10,13 @@ async function fetchImages (value, page=1){
         safesearch : true,
         per_page : 40,
         page : page,
-    }
-  }).then((resp) => {
+    })
+    const URL = `${BASE_URL}?${params}`;
+    return axios.get(URL, params).then((resp) => {
         if(!resp.ok){
             throw new Error(resp.status);
         }
         return resp.json();
-    })
+    });
 }
 export {fetchImages}
